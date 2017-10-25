b1773783e6b493a4972ef3ae5b85b20d9da5fb98
Stefano Suman <ssuman@whoi.edu>
2017-10-25 13:40:15 -0400

    Added ds_callbackqueue library that inherits from ROS CallbackQueue class and reimlements the addCallback virtual function, so that it could post events to an external event loop. Compiles but untested

---
+++ src/ds_callbackqueue.cpp
@@ -0,0 +1,47 @@
+#include "ros/callback_queue.h"
+
+
+namespace ros
+{
+  class ROSCPP_DECL DsCallbackQueue : public CallbackQueue
+  {
+  public:
+    virtual void addCallback(const CallbackInterfacePtr& callback, uint64_t removal_id = 0);
+  };
+}
+
+namespace ros
+{
+  void DsCallbackQueue::addCallback(const CallbackInterfacePtr& callback, uint64_t removal_id)
+  {
+    CallbackInfo info;
+    info.callback = callback;
+    info.removal_id = removal_id;
+
+    {
+      boost::mutex::scoped_lock lock(id_info_mutex_);
+
+      M_IDInfo::iterator it = id_info_.find(removal_id);
+      if (it == id_info_.end())
+	{
+	  IDInfoPtr id_info(boost::make_shared<IDInfo>());
+	  id_info->id = removal_id;
+	  id_info_.insert(std::make_pair(removal_id, id_info));
+	}
+    }
+
+    {
+      boost::mutex::scoped_lock lock(mutex_);
+
+      if (!enabled_)
+	{
+	  return;
+	}
+
+      callbacks_.push_back(info);
+    }
+
+    condition_.notify_one();
+    // SS - add posting of event to boost::asio io_service here
+  }
+}

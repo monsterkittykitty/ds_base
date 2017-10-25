ec7539d3b9ebd5010e8af2937191e905b909c2cf
Stefano Suman <ssuman@whoi.edu>
2017-10-25 13:24:02 -0400

    More experiments on spinners

--- src/dsl_process.cpp
+++ src/dsl_process.cpp
@@ -1,9 +1,18 @@
 #include <dsl_process.h>
+#include "ros/callback_queue.h"
 
 DslProcess::DslProcess():
   spinner(4),
   socket_(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 55555))
 {
+  ros::CallbackQueue* queue = ros::getGlobalCallbackQueue();
+  
+  ros::WallDuration timeout(0.1f);
+  while (nh.ok())
+    {
+      //queue->callAvailable(timeout);
+      queue->callAvailable();
+    }
   ROS_INFO_STREAM("Hello, world!");
   spinner.start();
   io_service.run();

082475a3faf2b80d8bc7340951696dda0450ed8c
Stefano Suman <ssuman@whoi.edu>
2017-11-07 12:00:11 -0500

    Prototyped array of connections and DsAsio wrappers of ros subscriptions and timers. Compiles and runs

--- include/ds_base/ds_asio.h
+++ include/ds_base/ds_asio.h
@@ -3,6 +3,8 @@
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/asio.hpp>
+//#include <ds_connection.h>
+#include <ds_udp.h>
 
 class DsAsio
 {
@@ -10,8 +12,14 @@ public:
   DsAsio();
   ~DsAsio();
 
+  template<typename T>
+    void addRosSubscription(std::string channel, int queue, void (*callback)(T&));
+  void addRosTimer(ros::Duration interval, void (*callback)(const ros::TimerEvent&));
+  
 private:
   boost::asio::io_service io_service;
   ros::NodeHandle   nh;
 
+  std::vector<DsConnection*> connections;
+  
 };
--- src/ds_asio.cpp
+++ src/ds_asio.cpp
@@ -1,5 +1,4 @@
 #include <ds_asio.h>
-#include <ds_udp.h>
 #include <ds_callbackqueue.h>
 #include "std_msgs/String.h"
 
@@ -9,28 +8,35 @@ void testCallback(const std_msgs::String::ConstPtr& msg)
   ROS_INFO_STREAM("Callback!");
 }
 
-void testCallback2(void)
-{
-  ROS_INFO_STREAM("Callback2!");
-}
-
 void timerCallback(const ros::TimerEvent&)
 {
   ROS_INFO_STREAM("Timer callback!");
 }
 
+template<typename T>
+void DsAsio::addRosSubscription(std::string channel, int queue, void (*callback)(T&))
+{
+  ros::Subscriber sub = nh.subscribe(channel, queue, callback);
+}
+
+void DsAsio::addRosTimer(ros::Duration interval, void (*callback)(const ros::TimerEvent&))
+{
+  ros::Timer timer = nh.createTimer(interval, callback);
+}
 
 DsAsio::DsAsio()
 {
 
-  DsUdp server(io_service);
-  server.receive();
+  connections.push_back(new DsUdp(io_service));
+  //DsUdp server(io_service);
+  connections[0]->receive();
 
   ros::DsCallbackQueue* queue = new ros::DsCallbackQueue(&io_service);
   nh.setCallbackQueue((ros::CallbackQueue*) queue);
 
-  ros::Timer timer = nh.createTimer(ros::Duration(0.5), timerCallback);
-  
+  addRosTimer(ros::Duration(0.5), timerCallback);
+  //ros::Timer timer = nh.createTimer(ros::Duration(0.5), timerCallback);
+  //addRosSubscription<const std_msgs::String::ConstPtr& msg>("test", 1000, (void *)&testCallback);
   ros::Subscriber sub = nh.subscribe("test", 1000, &testCallback);
   
   // Work object prevents io_service from quitting while it exists

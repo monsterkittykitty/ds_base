d3c5bb832a8af74963a99313dd7d0aa698e256ad
Stefano Suman <ssuman@whoi.edu>
2017-11-09 14:55:48 -0500

    Cleaned up ds_udp.cpp

--- src/ds_udp.cpp
+++ src/ds_udp.cpp
@@ -23,22 +23,17 @@ void DsUdp::handle_receive(const boost::system::error_code& error,
 {
   if (!error || error == boost::asio::error::message_size)
     {
-      boost::shared_ptr<std::string> message(
-					     new std::string("TEST"));
-
-      socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-			    boost::bind(&DsUdp::send, this, message,
-					boost::asio::placeholders::error,
-					boost::asio::placeholders::bytes_transferred));
-
       receive(callback_);
     }
 }
 
-void DsUdp::send(boost::shared_ptr<std::string> /*message*/,
-			 const boost::system::error_code& /*error*/,
-			 std::size_t /*bytes_transferred*/)
+void DsUdp::send(boost::shared_ptr<std::string> message,
+		 const boost::system::error_code& error,
+		 std::size_t bytes_transferred)
 {
-  ROS_INFO_STREAM("Sending UDP reply");
+  socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
+			boost::bind(&DsUdp::send, this, message,
+				    boost::asio::placeholders::error,
+				    boost::asio::placeholders::bytes_transferred));
 }
 

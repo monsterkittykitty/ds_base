c97d994f5576ae0b5a25ccde4da21567f3ff7e42
Stefano Suman <ssuman@whoi.edu>
2017-12-13 14:42:34 -0500

    DsNodeHandle successfully compiles. Only one of the 4 constructors of NodeHandle implemented so far

--- include/ds_base/ds_nodehandle.h
+++ include/ds_base/ds_nodehandle.h
@@ -1,6 +1,19 @@
 #ifndef DS_NODEHANDLE_H
 #define DS_NODEHANDLE_H
 
+#include "ds_base/ds_callbackqueue.h"
+#include "ros/ros.h"
 
+namespace ros
+{
+  class ROSCPP_DECL DsNodeHandle : public NodeHandle
+  {
+  public:
+    DsNodeHandle(boost::asio::io_service *io_service, const std::string &ns=std::string(), const M_string &remappings=M_string());
 
+  private:
+    boost::asio::io_service *myIoService;
+    ros::DsCallbackQueue    *queue;
+  };
+}
 #endif
--- src/ds_nodehandle.cpp
+++ src/ds_nodehandle.cpp
@@ -1,1 +1,12 @@
 #include "ds_base/ds_nodehandle.h"
+
+namespace ros
+{
+  DsNodeHandle::DsNodeHandle(boost::asio::io_service *io_service, const std::string &ns, const M_string &remappings):
+    NodeHandle(ns, remappings),
+    myIoService(io_service)
+  {
+    queue = new ros::DsCallbackQueue(io_service);
+    this->setCallbackQueue((ros::CallbackQueue*) queue);
+  }
+}

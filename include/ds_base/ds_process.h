44769d8ea0bf08da5a92cf2a10e5c00a9dbfdda7
Zac Berkowitz <zberkowitz@whoi.edu>
2018-01-08 16:49:43 -0500

    Added lots of helpers, getters/setters, etc.

--- include/ds_base/ds_process.h
+++ include/ds_base/ds_process.h
@@ -47,10 +47,137 @@ public:
   /// A period < 0 disables health checks.
   void setStatusCheckPeriod(ros::Duration period) noexcept;
 
+  /// @brief Give the node a descriptive name
+  ///
+  /// \param name
+  void setDescriptiveName(const std::string& name) noexcept;
+
+  /// @brief Get the node's descrpitive name;
+  ///
+  /// \return
+  std::string descriptiveName() const noexcept;
+
+
+  /// @brief Convenience function for adding a publisher
+  ///
+  /// Relative topic names (names not starting with '/') are appended to the node's name.  **HOWEVER!** The publisher
+  /// object is STILL stored using the original passed topic.  Let's have a few examples:
+  ///
+  ///
+  /// node name:  sensor
+  /// topic name: data
+  ///   --> messages will be published on 'sensor/data'
+  ///   --> publisher can be retrieved by getting publisher_.at('data')
+  ///
+  /// node name: sensor
+  /// topic name: /absolute
+  ///   --> messages will be published on '/absolute'
+  ///   --> publisher can be retrieved by getting publisher_at('/absolute')
+  ///
+  /// \tparam T      ROS message type
+  /// \param base    The owning SensorBase instance.
+  /// \param topic   Name of the topic to publish on
+  /// \param queue   Size of the publishing queue
+  template <class T>
+  void addPublisher(const std::string& topic, uint32_t queue)
+  {
+    // do nothing for empty strings...
+    if (topic.empty())
+    {
+      return;
+    }
+
+    if (publishers_.find(topic) != publishers_.end()) {
+      ROS_ERROR_STREAM("Unable to add publisher named: " << topic << ". A publisher on that topic already exists.");
+      return;
+    }
+
+    // construct our topic name and create the publisher.
+    auto full_topic_name = topic.at(0) == '/' ? topic : ros::this_node::getName() + '/' + topic;
+    publishers_[topic] = getNh()->advertise<T>(full_topic_name, queue);
+
+    ROS_INFO_STREAM("New topic: " << full_topic_name);
+  };
+
+  /// @brief Get a publisher object created by addPublisher
+  ///
+  /// Note:  An invalid publisher is returned if no publisher exists for the provided topic.
+  ///        This *shouldn't* crash things, ros::Publisher inclues it's own validity checks
+  ///        before publishing, but they're all private methods and we're unable to access
+  ///        them.
+  ///
+  /// \param topic
+  /// \param valid  True if the returned ros::Publisher is valid.
+  /// \return
+  ros::Publisher publisher(const std::string& topic, bool *valid=nullptr)
+  {
+    auto it = publishers_.find(topic);
+    if(it == publishers_.end()) {
+      ROS_ERROR_STREAM("No publisher available for topic name: " << topic);
+      if(valid != nullptr) {
+        *valid = false;
+      }
+      return {};
+    }
+
+    if (valid != nullptr) {
+      *valid = true;
+    }
+
+    return it->second;
+  }
+
+  /// @brief Add an asio-based (serial, udp, etc.) connection.
+  ///
+  /// \param name
+  /// \param callback
+  /// \return
+  boost::shared_ptr<DsConnection> addConnection(const std::string& name, boost::function<void(ds_core_msgs::RawData)> callback);
+
+  /// @brief Get a DsConnection object for a connection added previously by addConnection
+  ///
+  /// \param name
+  /// \return
+  boost::shared_ptr<DsConnection> connection(const std::string& name);
+
  protected:
   std::unique_ptr<DsAsio> myAsio;
   std::unique_ptr<ros::DsNodeHandle> nh;
 
+  /// @brief Setup node after ros has been initialized.
+  ///
+  /// This method is called in DsProcess' constructors, after the object has
+  /// been instantiated.  It is the main entry point to add run-time configuration
+  /// that requires a rosmaster to be running.
+  ///
+  /// Default implmementation calls, in order:
+  ///
+  /// - setupParameters()
+  /// - setupConnections()
+  /// - setupSubscriptions()
+  /// - setupPublishers()
+  ///
+  virtual void setup();
+
+  /// @brief Get parameters from server
+  ///
+  /// The default implementation looks for the following PRIVATE parameters:
+  ///  - health_check_period [double]
+  ///  - descriptive_name [string]
+  virtual void setupParameters();
+
+  /// @brief Create asio connections
+  virtual void setupConnections() {}
+
+  /// @brief Create ros topic subscriptions.
+  virtual void setupSubscriptions() {}
+
+  /// @brief Create ros topic publishers.
+  ///
+  /// The default implementation creates the following publishers:
+  ///  - status  [ds_core_msgs::Status]
+  virtual void setupPublishers();
+  std::unordered_map<std::string, ros::Publisher> publishers_;
   /// @brief Check the process status.
   ///
   /// This method is triggered by the status check timer.  The default
@@ -64,6 +191,7 @@ public:
 
   ros::Duration status_check_period_;   //!< The period for the status health timer (<0 disables)
   ros::Timer status_check_timer_;       //!< The status health timer itself.
+  std::string descriptive_node_name_;   //!< A short, descriptive name given to the process.
 
 };
 

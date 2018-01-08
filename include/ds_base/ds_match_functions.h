d7ada3316852f1e6749f6fa04f4470e75a8d213b
Stefano Suman <ssuman@whoi.edu>
2018-01-08 10:45:26 -0500

    Added two more matcher functions, one is a passthrough, the other is match_header_length to use with binary data. match_header_length implementation is incomplete in this commit

--- include/ds_base/ds_match_functions.h
+++ include/ds_base/ds_match_functions.h
@@ -1,8 +1,10 @@
 #ifndef DS_MATCH_FUNCTIONS_H
 #define DS_MATCH_FUNCTIONS_H
 
-//typedef boost::asio::buffers_iterator< boost::asio::streambuf::const_buffers_type > Iterator;
-
+/// @brief Generates a matcher class for async_read_until. This class makes async_read_until return when the delimiter matches the char c
+///
+/// @return iterator and true pair if the delimiter is identified, iterator and false pair if the delimiter is not identified
+///
 class match_char
 {
 public:
@@ -28,6 +30,64 @@ private:
 namespace boost{
   namespace asio {
     template <> struct is_match_condition<match_char>
+      : public boost::true_type {};
+  } // namespace asio
+} // namespace boost
+
+/// @brief Generates a matcher class for async_read_until. This class makes async_read_until return when the binary header is identified and the binary data received matches the expected length
+///
+/// @return iterator and true pair if the data packet is complete, iterator and false pair otherwise
+///
+class match_header_length
+{
+public:
+  explicit match_header_length(std::vector<unsigned char> header, int length) : header_(header), length_(length) {}
+
+  typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+
+  template <typename Iterator>
+  std::pair<Iterator, bool> operator()(
+      Iterator begin, Iterator end) const
+  {
+    ;
+  }
+
+private:
+  std::vector<unsigned char> header_;
+  int length_;
+};
+
+namespace boost{
+  namespace asio {
+    template <> struct is_match_condition<match_header_length>
+      : public boost::true_type {};
+  } // namespace asio
+} // namespace boost
+
+
+/// @brief Generates a matcher class for async_read_until. This class makes async_read_until return with every byte
+///
+/// @return iterator and true pair, meaning that the match condition is always true
+///
+class passthrough
+{
+public:
+  explicit passthrough() {}
+
+  typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+
+  template <typename Iterator>
+  std::pair<Iterator, bool> operator()(
+      Iterator begin, Iterator end) const
+  {
+    return std::make_pair(i, true);
+  }
+
+};
+
+namespace boost{
+  namespace asio {
+    template <> struct is_match_condition<passthrough>
       : public boost::true_type {};
   } // namespace asio
 } // namespace boost

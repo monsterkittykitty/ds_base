51a39cc9f28d61cec58fd411b166965efa93f9d4
Stefano Suman <ssuman@whoi.edu>
2018-05-03 14:45:40 -0300

    Added unit tests for matcher functions match_char and match_header_pd0. Tests pass

--- ds_asio/test/test_ds_match_functions.cpp
+++ ds_asio/test/test_ds_match_functions.cpp
@@ -39,6 +39,93 @@ TEST_F(MatchFunctionsTest, match_char)
   ASSERT_EQ(found, true);
 }
 
+TEST_F(MatchFunctionsTest, match_header_pd0)
+{
+  boost::asio::streambuf b;
+  std::ostream os(&b);
+
+  std::vector<unsigned char> myData;
+  myData.push_back(0x7f);
+  myData.push_back(0x7f);
+  myData.push_back(0x04);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+
+  os.write((const char *) myData.data(), myData.size() * sizeof(unsigned char));
+
+  auto obj = match_header_pd0();
+
+  typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+
+  iterator item;
+  bool found;
+  auto size = b.size();
+  auto start = boost::asio::buffers_begin(b.data());
+  auto end = boost::asio::buffers_end(b.data());
+  std::tie(item, found) = obj(start, end);
+  
+  ASSERT_EQ(found, true);
+}
+
+TEST_F(MatchFunctionsTest, match_header_pd0_fail)
+{
+  boost::asio::streambuf b;
+  std::ostream os(&b);
+
+  std::vector<unsigned char> myData;
+  myData.push_back(0x7f);
+  myData.push_back(0x7f);
+  myData.push_back(0x08);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+
+  os.write((const char *) myData.data(), myData.size() * sizeof(unsigned char));
+
+  auto obj = match_header_pd0();
+
+  typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+
+  iterator item;
+  bool found;
+  auto size = b.size();
+  auto start = boost::asio::buffers_begin(b.data());
+  auto end = boost::asio::buffers_end(b.data());
+  std::tie(item, found) = obj(start, end);
+  
+  ASSERT_EQ(found,false);
+}
+
+TEST_F(MatchFunctionsTest, match_header_pd0_0length)
+{
+  boost::asio::streambuf b;
+  std::ostream os(&b);
+
+  std::vector<unsigned char> myData;
+  myData.push_back(0x7f);
+  myData.push_back(0x7f);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+  myData.push_back(0x00);
+
+  os.write((const char *) myData.data(), myData.size() * sizeof(unsigned char));
+
+  auto obj = match_header_pd0();
+
+  typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+
+  iterator item;
+  bool found;
+  auto size = b.size();
+  auto start = boost::asio::buffers_begin(b.data());
+  auto end = boost::asio::buffers_end(b.data());
+  std::tie(item, found) = obj(start, end);
+  
+  ASSERT_EQ(found, true);
+}
+
 // Run all the tests that were declared with TEST()
 int main(int argc, char** argv)
 {

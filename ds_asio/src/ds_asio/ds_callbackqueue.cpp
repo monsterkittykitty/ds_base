6d9d433b45116bd2fab95721f4b97d2f8485587d
Ian Vaughn <ivaughn@whoi.edu>
2019-08-08 14:45:45 -0400

    Removed copy/pasted code and replaced with superclass call to improve maintainability

--- ds_asio/src/ds_asio/ds_callbackqueue.cpp
+++ ds_asio/src/ds_asio/ds_callbackqueue.cpp
@@ -37,34 +37,8 @@ DsCallbackQueue::DsCallbackQueue(boost::asio::io_service* io_service) : ros::Cal
 
 void DsCallbackQueue::addCallback(const ros::CallbackInterfacePtr& callback, uint64_t removal_id)
 {
-  CallbackInfo info;
-  info.callback = callback;
-  info.removal_id = removal_id;
-
-  {
-    boost::mutex::scoped_lock lock(id_info_mutex_);
-
-    M_IDInfo::iterator it = id_info_.find(removal_id);
-    if (it == id_info_.end())
-    {
-      IDInfoPtr id_info(boost::make_shared<IDInfo>());
-      id_info->id = removal_id;
-      id_info_.insert(std::make_pair(removal_id, id_info));
-    }
-  }
-
-  {
-    boost::mutex::scoped_lock lock(mutex_);
-
-    if (!enabled_)
-    {
-      return;
-    }
-
-    callbacks_.push_back(info);
-  }
-
-  condition_.notify_one();
+  // call baseclass implementation
+  ros::CallbackQueue::addCallback(callback, removal_id);
 
   // SS - add posting of event to boost::asio io_service here
   myIoService->post(boost::bind(&ds_asio::DsCallbackQueue::callAvailable, this));
